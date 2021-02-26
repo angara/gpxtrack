62fba0d0eff238619123436b82e194e202ed8dae
Maxim Penzin <maxp@ya.ru>
2021-02-26 19:20:46 +0800

    deps update

--- srv/dev/user.clj
+++ srv/dev/user.clj
@@ -1,12 +1,12 @@
 (ns user
   (:require
-    [clojure.tools.namespace.repl :as tnr]
+   [clojure.tools.namespace.repl :as tnr]
     ;[criterium.core :refer [quick-bench]]
-    [mount.core :as mount]
-    [hashp.core]
+   [mount.core :as mount]
+   [hashp.core]
     ;    
-    [gpt.main  :refer [-main]]
-    [gpt.db.migration :as mg]))
+   [gpt.main  :refer [-main]]
+   [gpt.db.migration :as mg]))
 ;=
 
 (-main)
@@ -20,7 +20,7 @@
 
 
 (comment
-  
+
 ;  (try (restart) (catch Exception _ _))
 
   (mount/start)
@@ -28,8 +28,19 @@
   (restart)
   (reset)
 
-  (mg/rollback)
-  
-  ,)
+  (mg/rollback))
+
+;; (defmacro my-> [v & [f & t]]
+;;   (cond
+;;     (not f) v
+;;     (seq? f)
+;;     (let [[ff & tt] f]
+;;       `(my-> (~ff ~v ~@tt) ~@t))
+;;     :else
+;;     `(my->
+;;       (~f ~v)
+;;       ~@t)))
+;;
+;; (macroexpand `(my-> 1 (inc) (str 2)))
 
 ;;.
